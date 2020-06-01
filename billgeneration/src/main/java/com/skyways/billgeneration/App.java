162085c5276808e7dc382c52ed66ff14ad0376a2
Chowdhary <52133240@acsind.com>
2020-06-01 21:12:17 +0530

    Initial commit

---
+++ billgeneration/src/main/java/com/skyways/billgeneration/App.java
@@ -0,0 +1,13 @@
+package com.skyways.billgeneration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
